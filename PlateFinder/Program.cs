8326e98e8ad3ff5809d021772206a673d4aa2004
Pairs34 <34889012+Pairs34@users.noreply.github.com>
2020-10-07 22:07:00 +0300

    Add project files.

---
+++ PlateFinder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using openalprnet;
+using System.Linq;
+
+namespace PlateFinder
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var openalprConf = Path.Combine(Directory.GetCurrentDirectory(), "openalpr.conf");
+            var runtimeData = Path.Combine(Directory.GetCurrentDirectory(), "runtime_data");
+            var samplePath = Path.Combine(Directory.GetCurrentDirectory(),"samples\\2.jpg");
+            var alpr = new AlprNet("eu",
+                openalprConf,
+                runtimeData);
+            if (!alpr.IsLoaded())
+            {
+                Console.WriteLine("OpenAlpr failed to load!");
+                return;
+            }
+
+            var results = alpr.Recognize(samplePath);
+            Console.WriteLine(results.Plates.Select(x => x.BestPlate.Characters).FirstOrDefault());
+
+            Console.ReadLine();
+        }
+    }
+}
